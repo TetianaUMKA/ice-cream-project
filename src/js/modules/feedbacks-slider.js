f120fd3c06f3600e6932f4ee2ee2a74e590e1fb9
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-11-28 18:59:49 +0200

    Add swipe to mob feedbacks slider

--- src/js/modules/feedbacks-slider.js
+++ src/js/modules/feedbacks-slider.js
@@ -1,5 +1,6 @@
 const items = document.querySelectorAll('.feedbacks-item');
 const dots = document.querySelectorAll('.dot-btn');
+const slider = document.querySelector('.feedbacks-list');
 
 dots.forEach((dot, index) => {
   dot.addEventListener('click', () => {
@@ -10,3 +11,33 @@ dots.forEach((dot, index) => {
     dot.classList.add('active');
   });
 });
+
+let startX = 0;
+
+slider.addEventListener('touchstart', e => {
+  startX = e.touches[0].clientX;
+});
+
+slider.addEventListener('touchend', e => {
+  const endX = e.changedTouches[0].clientX;
+  const diff = startX - endX;
+  const swipeDistance = 50;
+
+  if (diff > swipeDistance) {
+    goToNextSlide();
+  } else if (diff < -swipeDistance) {
+    goToPrevSlide();
+  }
+});
+
+function goToNextSlide() {
+  let nextIndex = currentIndex + 1;
+  if (nextIndex >= items.length) nextIndex = 0;
+  showSlide(nextIndex);
+}
+
+function goToPrevSlide() {
+  let prevIndex = currentIndex - 1;
+  if (prevIndex < 0) prevIndex = items.length - 1;
+  showSlide(prevIndex);
+}
